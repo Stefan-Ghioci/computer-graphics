7a1991a421e84b33d7c202d47144b3fb9c868c02
Stefan Ghioci <ghioci.stefan@gmail.com>
2019-12-03 17:02:45 +0200

    Draw a triangle on a colored background

--- opengl_simple/Source.cpp
+++ opengl_simple/Source.cpp
@@ -1,51 +1,101 @@
+// Include standard headers
 #include <cstdio>
 #include <cstdlib>
-#include <GL/glew.h>
-#include <GLFW/glfw3.h>
 
+// Include GLEW
+#include <GL/glew.h>
+
+// Include GLFW
+#include <GLFW/glfw3.h>
+GLFWwindow* window;
 
 int main()
 {
-	glewExperimental = true;
+	// Initialise GLFW
 	if (!glfwInit())
 	{
 		fprintf(stderr, "Failed to initialize GLFW\n");
+		getchar();
 		return -1;
 	}
 
 	glfwWindowHint(GLFW_SAMPLES, 4);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	auto* window = glfwCreateWindow(1024, 768, "Tutorial 01", nullptr, nullptr);
+	// Open a window and create its OpenGL context
+	window = glfwCreateWindow(1024, 768, "Triangle", nullptr, nullptr);
 	if (window == nullptr)
 	{
-		fprintf(
-			stderr,
-			"Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible. Try the 2.1 version of the tutorials.\n");
+		fprintf(stderr, "Failed to open GLFW window\n");
+		getchar();
 		glfwTerminate();
 		return -1;
 	}
-	
 	glfwMakeContextCurrent(window);
-	glewExperimental = true;
-	
+
+	// Initialize GLEW
+	glewExperimental = true; // Needed for core profile
 	if (glewInit() != GLEW_OK)
 	{
 		fprintf(stderr, "Failed to initialize GLEW\n");
+		getchar();
+		glfwTerminate();
 		return -1;
 	}
 
+	// Ensure we can capture the escape key being pressed below
 	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+
+	// Background
+	glClearColor(0.5f, 0.2f, 0.8f, 0.0f);
+
+	GLuint vertex_array_id;
+	glGenVertexArrays(1, &vertex_array_id);
+	glBindVertexArray(vertex_array_id);
+
+	// Triangle positions X,Y,Z
+	static const GLfloat g_vertex_buffer_data[] = {
+		-0.5f, -0.5f, 1.0f,
+		0.5f, -0.5f, -1.0f,
+		0.0f, 0.5f, -1.0f,
+	};
+
+	GLuint vertexbuffer;
+	glGenBuffers(1, &vertexbuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof g_vertex_buffer_data, g_vertex_buffer_data, GL_STATIC_DRAW);
 
 	do
 	{
+		// Clear the screen
 		glClear(GL_COLOR_BUFFER_BIT);
+
+
+		// 1rst attribute buffer : vertices
+		glEnableVertexAttribArray(0);
+		glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+		glVertexAttribPointer(0, 3,GL_FLOAT,GL_FALSE, 0, nullptr);
+
+		// Draw the triangle !
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+
+		glDisableVertexAttribArray(0);
+
+		// Swap buffers
 		glfwSwapBuffers(window);
 		glfwPollEvents();
-	}
+	} // Check if the ESC key was pressed or the window was closed
 	while (glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS &&
 		glfwWindowShouldClose(window) == 0);
+
+	// Cleanup VBO
+	glDeleteBuffers(1, &vertexbuffer);
+	glDeleteVertexArrays(1, &vertex_array_id);
+
+	// Close OpenGL window and terminate GLFW
+	glfwTerminate();
+
+	return 0;
 }
